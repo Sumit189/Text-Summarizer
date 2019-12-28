853c0d9c48657d456cef7fa1e8993c545f405d19
Sumit <sumit.18.paul@gmail.com>
2019-12-28 19:02:52 +0530

    Second commit

--- application.py
+++ application.py
@@ -21,48 +21,47 @@ def home():
       text = request.form['original_text'] 
       return render_template('Summarized.html',name=create_summary(text)) 
   
-# def create_summary(text):
-#     corpus=text.lower().split('.')
-#     formatted_text=re.sub(r'\[[0-9]*\]',' ',str(corpus))
-#     formatted_text=re.sub(r'\s+',' ',str(corpus))
-#     formatted_text=re.sub('[^a-zA-Z]',' ',str(corpus))
-#     formatted_text=re.sub('\s+',' ',str(formatted_text))
-#     sentence_list=nltk.sent_tokenize(str(text))
-#     words_freq=word_frequency(formatted_text)
-#     max_freq=max(words_freq.values())
-#     #weight finding
-#     for word in words_freq.keys():
-#         words_freq[word]=(words_freq[word]/max_freq)
-#     score=compute_score(sentence_list,words_freq)
-#     #summary
-#     summary_sentence=heapq.nlargest(math.ceil(len(corpus)*0.30),score,key=score.get)
-#     summary=' '.join(summary_sentence)
-#     return summary
+def create_summary(text):
+    corpus=text.lower().split('.')
+    formatted_text=re.sub(r'\[[0-9]*\]',' ',str(corpus))
+    formatted_text=re.sub(r'\s+',' ',str(corpus))
+    formatted_text=re.sub('[^a-zA-Z]',' ',str(corpus))
+    formatted_text=re.sub('\s+',' ',str(formatted_text))
+    sentence_list=nltk.sent_tokenize(str(text))
+    words_freq=word_frequency(formatted_text)
+    max_freq=max(words_freq.values())
+    #weight finding
+    for word in words_freq.keys():
+        words_freq[word]=(words_freq[word]/max_freq)
+    score=compute_score(sentence_list,words_freq)
+    #summary
+    summary_sentence=heapq.nlargest(math.ceil(len(corpus)*0.30),score,key=score.get)
+    summary=' '.join(summary_sentence)
+    return summary
 
 
-# def word_frequency(formatted_text):
-#     #freq finding
-#     words_freq={}
-#     for word in nltk.word_tokenize(formatted_text):
-#         if word not in stop_words:
-#             if word not in words_freq.keys():
-#                 words_freq[word]=1
-#             else:
-#                 words_freq[word]+=1    
-#     return words_freq
+def word_frequency(formatted_text):
+    #freq finding
+    words_freq={}
+    for word in nltk.word_tokenize(formatted_text):
+        if word not in stop_words:
+            if word not in words_freq.keys():
+                words_freq[word]=1
+            else:
+                words_freq[word]+=1    
+    return words_freq
 
-# def compute_score(sentence_list,words_freq):
-#     sentence_score={}
-#     for sent in sentence_list:
-#         for word in nltk.word_tokenize(sent.lower()):
-#             if word in words_freq.keys():
-#                 if len(sent.split(' '))<30:
-#                     if sent not in sentence_score.keys():
-#                         sentence_score[sent]=words_freq[word]
-#                     else:
-#                         sentence_score[sent]+=words_freq[word]
-#     return sentence_score
+def compute_score(sentence_list,words_freq):
+    sentence_score={}
+    for sent in sentence_list:
+        for word in nltk.word_tokenize(sent.lower()):
+            if word in words_freq.keys():
+                if len(sent.split(' '))<30:
+                    if sent not in sentence_score.keys():
+                        sentence_score[sent]=words_freq[word]
+                    else:
+                        sentence_score[sent]+=words_freq[word]
+    return sentence_score
 
 if __name__ == '__main__':
-   application.debug=True
-   application.run()   
+   application.run(host='0.0.0.0', debug=True, port=5000)   
